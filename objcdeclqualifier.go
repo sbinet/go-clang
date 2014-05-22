63bb80d5358e099849a2d93c4cab21304b8cab60
Sebastien Binet <binet@cern.ch>
2014-05-22 18:38:47 +0200

    objc: stubs for objective-C

---
+++ objcdeclqualifier.go
@@ -0,0 +1,16 @@
+package clang
+
+// TODO: Objective-C support
+// /**
+//  * \brief 'Qualifiers' written next to the return and parameter types in
+//  * ObjC method declarations.
+//  */
+// typedef enum {
+//   CXObjCDeclQualifier_None = 0x0,
+//   CXObjCDeclQualifier_In = 0x1,
+//   CXObjCDeclQualifier_Inout = 0x2,
+//   CXObjCDeclQualifier_Out = 0x4,
+//   CXObjCDeclQualifier_Bycopy = 0x8,
+//   CXObjCDeclQualifier_Byref = 0x10,
+//   CXObjCDeclQualifier_Oneway = 0x20
+// } CXObjCDeclQualifierKind;
---
+++ objcpropertyattr.go
@@ -0,0 +1,21 @@
+package clang
+
+// TODO: Objective-C support
+// /**
+//  * \brief Property attributes for a \c CXCursor_ObjCPropertyDecl.
+//  */
+// typedef enum {
+//   CXObjCPropertyAttr_noattr    = 0x00,
+//   CXObjCPropertyAttr_readonly  = 0x01,
+//   CXObjCPropertyAttr_getter    = 0x02,
+//   CXObjCPropertyAttr_assign    = 0x04,
+//   CXObjCPropertyAttr_readwrite = 0x08,
+//   CXObjCPropertyAttr_retain    = 0x10,
+//   CXObjCPropertyAttr_copy      = 0x20,
+//   CXObjCPropertyAttr_nonatomic = 0x40,
+//   CXObjCPropertyAttr_setter    = 0x80,
+//   CXObjCPropertyAttr_atomic    = 0x100,
+//   CXObjCPropertyAttr_weak      = 0x200,
+//   CXObjCPropertyAttr_strong    = 0x400,
+//   CXObjCPropertyAttr_unsafe_unretained = 0x800
+// } CXObjCPropertyAttrKind;
