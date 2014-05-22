f64f5760914f9a8b7e1c142c469c54405bb41db2
Sebastien Binet <binet@cern.ch>
2014-05-22 19:08:26 +0200

    comment: add clang_Comment_getKind

--- comment.go
+++ comment.go
@@ -11,3 +11,12 @@ import "C"
 type Comment struct {
 	c C.CXComment
 }
+
+/**
+ * \param Comment AST node of any kind.
+ *
+ * \returns the type of the AST node.
+ */
+func (c Comment) Kind() CommentKind {
+	return CommentKind(C.clang_Comment_getKind(c.c))
+}
