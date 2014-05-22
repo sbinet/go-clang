5a86068c6e3f2eb4304f0e36431ae4bcfa5216e2
Sebastien Binet <binet@cern.ch>
2014-05-22 18:20:29 +0200

    type: impl clang_Type_getAlignOf + clang_Type_getClassType + clang_Type_getSizeOf + clang_Type_getOffsetOf

--- type.go
+++ type.go
@@ -9,6 +9,10 @@ package clang
 //
 import "C"
 
+import (
+	"unsafe"
+)
+
 // Type represents the type of an element in the abstract syntax tree.
 type Type struct {
 	c C.CXType
@@ -133,6 +137,75 @@ func (t Type) ArrayElementType() Type {
 func (t Type) ArraySize() int64 {
 	o := C.clang_getArraySize(t.c)
 	return int64(o)
+}
+
+/**
+ * \brief Return the alignment of a type in bytes as per C++[expr.alignof]
+ *   standard.
+ *
+ * If the type declaration is invalid, CXTypeLayoutError_Invalid is returned.
+ * If the type declaration is an incomplete type, CXTypeLayoutError_Incomplete
+ *   is returned.
+ * If the type declaration is a dependent type, CXTypeLayoutError_Dependent is
+ *   returned.
+ * If the type declaration is not a constant size type,
+ *   CXTypeLayoutError_NotConstantSize is returned.
+ */
+func (t Type) AlignOf() (int, error) {
+	o := C.clang_Type_getAlignOf(t.c)
+	if o < 0 {
+		return int(o), TypeLayoutError(o)
+	}
+	return int(o), nil
+}
+
+/**
+ * \brief Return the class type of an member pointer type.
+ *
+ * If a non-member-pointer type is passed in, an invalid type is returned.
+ */
+func (t Type) ClassType() Type {
+	return Type{C.clang_Type_getClassType(t.c)}
+}
+
+/**
+ * \brief Return the size of a type in bytes as per C++[expr.sizeof] standard.
+ *
+ * If the type declaration is invalid, CXTypeLayoutError_Invalid is returned.
+ * If the type declaration is an incomplete type, CXTypeLayoutError_Incomplete
+ *   is returned.
+ * If the type declaration is a dependent type, CXTypeLayoutError_Dependent is
+ *   returned.
+ */
+func (t Type) SizeOf() (int, error) {
+	o := C.clang_Type_getSizeOf(t.c)
+	if o < 0 {
+		return int(o), TypeLayoutError(o)
+	}
+	return int(o), nil
+}
+
+/**
+ * \brief Return the offset of a field named S in a record of type T in bits
+ *   as it would be returned by __offsetof__ as per C++11[18.2p4]
+ *
+ * If the cursor is not a record field declaration, CXTypeLayoutError_Invalid
+ *   is returned.
+ * If the field's type declaration is an incomplete type,
+ *   CXTypeLayoutError_Incomplete is returned.
+ * If the field's type declaration is a dependent type,
+ *   CXTypeLayoutError_Dependent is returned.
+ * If the field's name S is not found,
+ *   CXTypeLayoutError_InvalidFieldName is returned.
+ */
+func (t Type) OffsetOf(s string) (int, error) {
+	c_str := C.CString(s)
+	defer C.free(unsafe.Pointer(c_str))
+	o := C.clang_Type_getOffsetOf(t.c, c_str)
+	if o < 0 {
+		return int(o), TypeLayoutError(o)
+	}
+	return int(o), nil
 }
 
 // EOF
