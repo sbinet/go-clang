bec7df51c887411a6d9eacd302be757d88094f9b
Sebastien Binet <binet@cern.ch>
2014-05-22 19:39:45 +0200

    completion: add clang_getCompletionParent + clang_getCompletionBriefComment

--- complete.go
+++ complete.go
@@ -85,6 +85,38 @@ func (cs CompletionString) Annotation(i int) string {
 }
 
 /**
+ * \brief Retrieve the parent context of the given completion string.
+ *
+ * The parent context of a completion string is the semantic parent of
+ * the declaration (if any) that the code completion represents. For example,
+ * a code completion for an Objective-C method would have the method's class
+ * or protocol as its context.
+ *
+ * \param completion_string The code completion string whose parent is
+ * being queried.
+ *
+ * \param kind DEPRECATED: always set to CXCursor_NotImplemented if non-NULL.
+ *
+ * \returns The name of the completion parent, e.g., "NSObject" if
+ * the completion string represents a method in the NSObject class.
+ */
+func (cs CompletionString) CompletionParent() string {
+	o := cxstring{C.clang_getCompletionParent(cs.c, nil)}
+	defer o.Dispose()
+	return o.String()
+}
+
+/**
+ * \brief Retrieve the brief documentation comment attached to the declaration
+ * that corresponds to the given completion string.
+ */
+func (cs CompletionString) CompletionBriefComment() string {
+	o := cxstring{C.clang_getCompletionBriefComment(cs.c)}
+	defer o.Dispose()
+	return o.String()
+}
+
+/**
  * \brief Retrieve the annotation associated with the given completion string.
  *
  * \param completion_string the completion string to query.
