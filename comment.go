e6b84e18173a412deba6ae8e164477c7cc863dac
Sebastien Binet <binet@cern.ch>
2014-05-22 19:13:38 +0200

    comment: add clang_Comment_getNumChildren + clang_Comment_getChild

--- comment.go
+++ comment.go
@@ -20,3 +20,23 @@ type Comment struct {
 func (c Comment) Kind() CommentKind {
 	return CommentKind(C.clang_Comment_getKind(c.c))
 }
+
+/**
+ * \param Comment AST node of any kind.
+ *
+ * \returns number of children of the AST node.
+ */
+func (c Comment) NumChildren() int {
+	return int(C.clang_Comment_getNumChildren(c.c))
+}
+
+/**
+ * \param Comment AST node of any kind.
+ *
+ * \param ChildIdx child index (zero-based).
+ *
+ * \returns the specified child of the AST node.
+ */
+func (c Comment) Child(idx int) Comment {
+	return Comment{C.clang_Comment_getChild(c.c, C.unsigned(idx))}
+}
