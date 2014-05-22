073a14f3eb05bd994c15012927d55d4da8bfec6b
Sebastien Binet <binet@cern.ch>
2014-05-22 14:45:11 +0200

    fid: first import

---
+++ fileuniqueid.go
@@ -0,0 +1,35 @@
+package clang
+
+// #include <stdlib.h>
+// #include "clang-c/Index.h"
+// #include "clang-c/CXCompilationDatabase.h"
+// #include "go-clang.h"
+//
+import "C"
+import
+
+/**
+ * \brief Uniquely identifies a CXFile, that refers to the same underlying file,
+ * across an indexing session.
+ */"fmt"
+
+type FileUniqueID struct {
+	c C.CXFileUniqueID
+}
+
+/**
+ * \brief Retrieve the unique ID for the given \c file.
+ *
+ * \param file the file to get the ID for.
+ * \param outID stores the returned CXFileUniqueID.
+ * \returns If there was a failure getting the unique ID, returns non-zero,
+ * otherwise returns 0.
+ */
+func (f File) GetFileUniqueID() (FileUniqueID, error) {
+	var fid FileUniqueID
+	o := C.clang_getFileUniqueID(f.c, &fid.c)
+	if o != 0 {
+		return fid, fmt.Errorf("clang: could not get FileUniqueID (err=%d)", o)
+	}
+	return fid, nil
+}
