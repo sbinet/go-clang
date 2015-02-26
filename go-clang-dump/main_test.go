737b1a329fd46c5fa91866a97db4bd181052d5a1
Sebastien Binet <binet@cern.ch>
2015-02-26 20:21:14 +0100

    tests: test go-clang-dump

---
+++ go-clang-dump/main_test.go
@@ -0,0 +1,16 @@
+package main_test
+
+import (
+	"os/exec"
+	"testing"
+)
+
+func TestClangDump(t *testing.T) {
+	for _, fname := range []string{"../testdata/hello.c", "../testdata/struct.c"} {
+		cmd := exec.Command("go-clang-dump", "-fname", fname)
+		err := cmd.Run()
+		if err != nil {
+			t.Fatalf("error running go-clang-dump on %q: %v\n", fname, err)
+		}
+	}
+}
