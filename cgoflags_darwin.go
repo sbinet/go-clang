4f99331b24b75355d7fde14647636ba302f727b6
Sebastien Binet <binet@cern.ch>
2015-02-27 00:17:12 +0100

    cgoflags: update LDFLAGS to pick-up llvm-3.5

--- cgoflags_darwin.go
+++ cgoflags_darwin.go
@@ -1,6 +1,6 @@
 package clang
 
-// #cgo darwin LDFLAGS: -L/opt/local/libexec/llvm-3.4/lib
+// #cgo darwin LDFLAGS: -L/opt/local/libexec/llvm-3.5/lib
 import "C"
 
 //EOF
--- cgoflags_droneio.go
+++ cgoflags_droneio.go
@@ -2,5 +2,5 @@
 
 package clang
 
-// #cgo linux LDFLAGS: -L/usr/lib/llvm-3.4/lib
+// #cgo linux LDFLAGS: -L/usr/lib/llvm-3.5/lib
 import "C"
